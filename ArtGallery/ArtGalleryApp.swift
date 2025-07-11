0b33cc072739d88be2f59d9cdc39333effee2536
SergeyZakurakin <pricezak@gmail.com>
2025-07-10 21:35:54 -0400

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Sergey Zakurakin on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
