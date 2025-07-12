7188ec5092f930ece8e7c48a9724c71b550eb031
SergeyZakurakin <pricezak@gmail.com>
2025-07-12 18:39:20 -0400

    upadate link

--- ArtGallery/WorkView.swift
+++ ArtGallery/WorkView.swift
@@ -29,8 +29,8 @@ struct WorkView: View {
                     )
                 
                 VStack(alignment: .leading) {
-                    Text("The Weeping Woman")
-                    Text("The Weeping Woman, (60 х 49 cm, 23 ⅝ х 19 ¼ inches) is an oil on canvas painted by Pablo Picasso in France in 1937. Picasso was intrigued with the subject, and revisited the theme numerous times that year. This painting was the final and most elaborate of the series. It has been in the collection of the Tate in London since 1987, and is on exhibition at the Tate Modern, London.")
+                    Text(works.title)
+                    Text(works.info)
                 }
                 Spacer()
                 
